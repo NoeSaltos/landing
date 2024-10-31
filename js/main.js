7a5aae116bac73a775af0ea75e09e5e83ca5551c
NoeSaltos <noeasalt@espol.edu.ec>
2024-10-30 19:06:50 -0500

    Cambios

--- js/main.js
+++ js/main.js
@@ -1,5 +1,99 @@
+const databaseURL = 'https://landing-51be0-default-rtdb.firebaseio.com/data.json';
+let sendData = () => { 
+    // Obtén los datos del formulario
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries()); // Convierte FormData a objeto
+    
+    // new Date().toLocaleString( locales, options )
+    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
+    
+    // Realiza la petición POST con fetch
+    fetch(databaseURL, {
+        method: 'POST', // Método de la solicitud
+        headers: {
+            'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
+        },
+        //Serializar(convertir a texto)
+        body: JSON.stringify(data) // Convierte los datos a JSON
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.statusText}`);
+        }
+        //Deserializar convertir el texto al objeto
+        return response.json(); // Procesa la respuesta como JSON
+    })
+    .then(result => {
+        alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+        form.reset()//hace que desparezcan los datos cada vez que se envian 
+        
+        // Recuperación de datos
+        getData();
+    })
+    .catch(error => {
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    });
+ }
+
+ let getData = async()=>{
+    try {
+
+        // Realiza la petición fetch a la URL de la base de datos
+        const response = await fetch(databaseURL);
+
+        // Verifica si la respuesta es exitosa
+        if (!response.ok) {
+          alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+        }
+
+        // Convierte la respuesta en formato JSON
+        const data = await response.json();
+
+        if(data != null) {
+
+            // Cuente el número de suscriptores registrados por fecha a partir del objeto data
+            let countSuscribers = new Map()
+            if (Object.keys(data).length > 0) {
+                for (let key in data) {
+
+                    let { email, saved } = data[key]
+                       
+                    let date = saved.split(",")[0]
+                       
+                    let count = countSuscribers.get(date) || 0;
+                    countSuscribers.set(date, count + 1)
+                }
+            }
+            // END
+
+            // Genere y agregue filas de una tabla HTML para mostrar fechas y cantidades de suscriptores almacenadas 
+            if (countSuscribers.size > 0) {
+
+                subscribers.innerHTML = ''
+
+                for (let [date, count] of countSuscribers) {
+                    let rowTemplate = `
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>${date}</td>
+                            <td>${count}</td>
+                        </tr>`
+                    subscribers.innerHTML += rowTemplate
+                }
+            }
+            // END
+
+        }
+
+      } catch (error) {
+        // Muestra cualquier error que ocurra durante la petición
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+      }
+ }
+
 let ready = () => {
     console.log('DOM está listo')
+    getData();
 }
 
 let loaded = () => {
@@ -26,10 +120,15 @@ let loaded = () => {
                 easing: "linear",
             }
         )
+        return;
+
         }
+        sendData();
+
     });
  
 }
+
 
 
 window.addEventListener("DOMContentLoaded", ready);
